73ef1afde37a1e1eb85698976e538e7148094286
WizzyPC <134875294+WizzyPC@users.noreply.github.com>
2025-07-15 19:33:13 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,20 +86,11 @@ export default async function RootLayout({
   );
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <Analytics />
-      </body>
-    </html>
+    <>
+      {children}
+      <Analytics />
+    </>
   );
 }
