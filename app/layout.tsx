6521c97266ff6d670d2e4aa820fca841545c7e9a
WizzyPC <134875294+WizzyPC@users.noreply.github.com>
2025-07-15 19:37:59 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { Toaster } from 'sonner';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/next';
 
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
